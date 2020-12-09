4666f314300ab539b125348a31ae6ccbf81f0a6f
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-12-10 01:09:25 +0600

    display circle data

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 import { Circle, Popup } from "react-leaflet";
+import numeral from "numeral";
 
 const caseTypeColors = {
     cases: {
@@ -40,7 +41,24 @@ export const showDataOnMap = (allData, caseType = "deaths") =>
             }
         >
             <Popup>
-                <h1> This is popup</h1>
+                <div className="info-container">
+                    <div
+                        className="info-flag"
+                        style={{
+                            backgroundImage: `url(${country.countryInfo.flag})`,
+                        }}
+                    />
+                    <div className="info-name">{country.country}</div>
+                    <div className="info-cases">
+                        Cases: {numeral(country.cases).format("0,0")}
+                    </div>
+                    <div className="info-recovered">
+                        Recovered: {numeral(country.recovered).format("0,0")}
+                    </div>
+                    <div className="info-deaths">
+                        Deaths: {numeral(country.deaths).format("0,0")}
+                    </div>
+                </div>
             </Popup>{" "}
         </Circle>
     ));
